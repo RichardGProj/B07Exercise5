12bd89f3d8e239693b83b33a5a25b1f2aa1ba572
9thAzure <dl6741@yahoo.ca>
2026-06-22 14:02:41 -0400

    feat: Add `Quadrilateral.java` class with constructor and fields.

---
+++ Quadrilateral.java
@@ -0,0 +1,12 @@
+
+public class Quadrilateral {
+	Point a, b, c, d;
+	
+	public Quadrilateral(Point a, Point b, Point c, Point d)
+	{
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+}
